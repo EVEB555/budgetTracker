f805100635763e285001981e93b3837d2397b20a
EVEB555 <97383055+EVEB555@users.noreply.github.com>
2023-05-17 19:51:07 +0300

    Initial commit

---
+++ src/main/java/com/app/budget/tracker/BudgetTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.app.budget.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetTrackerApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/com/app/budget/tracker/BudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.budget.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
